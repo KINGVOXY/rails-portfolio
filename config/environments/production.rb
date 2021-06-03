14f3a63d62c662da293666830107e93a4fc377ab
daruo <daruo0830@gmail.com>
2021-06-03 21:57:51 +0900

    プリコンパイルの設定など

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  config.assets.css_compressor = :sass
+  config.assets.enabled = true
+  config.assets.compile = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -63,7 +64,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rails-portfolio_#{Rails.env}"
 
+  config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:            ENV['EMAIL'],
+  password:             ENV['PASSKEY'],
+  authentication:       'plain',
+  enable_starttls_auto:  true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
