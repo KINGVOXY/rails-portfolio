4d6c9e57a8d935c07293285bdb5354b5dd15a818
daruo <daruo0830@gmail.com>
2021-06-03 21:58:27 +0900

    静的ページルーティングなど

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resource :contacts, only: [:new, :create]
+  root 'home#about'
+  
+  get '/contacts', to: 'contacts#new'
+  post '/contacts', to: 'contacts#create'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
