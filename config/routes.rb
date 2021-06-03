809bb36142c2ce6603b4f59bc73fff0c2a0553c0
daruo <daruo0830@gmail.com>
2021-06-04 01:59:48 +0900

    ルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  root 'home#about'
+  root    'home#about'                                  # トップページ
   
-  get '/contacts', to: 'contacts#new'
-  post '/contacts', to: 'contacts#create'
+  get     '/login',             to: 'sessions#new'      # ログインページ
+  post    '/login',             to: 'sessions#create'   # ログイン処理
+  delete  '/logout',            to: 'sessions#destroy'  # ログアウト
+  
+  get     '/blog',              to: 'blog#index'        # ブログ一覧
+  get     '/blog/new',          to: 'blog#new'          # 記事作成
+  post    '/blog/create',       to: 'blog#create'       # 投稿処理
+  get     '/blog/:id/edit',     to: 'blog#edit'         # 記事編集
+  get     '/blog/:id/update',   to: 'blog#update'       # 記事更新
+  destroy '/blog/:id/destroy',  to: 'blog#destroy'      # 記事削除
+  
+  get     '/contacts',          to: 'contacts#new'      # 問い合わせページ
+  post    '/contacts',          to: 'contacts#create'   # 問い合わせ完了処理
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
