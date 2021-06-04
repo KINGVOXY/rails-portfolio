2d8f17271893ec077e83d3413d6f944ec9aaca03
daruo <daruo0830@gmail.com>
2021-06-04 22:01:01 +0900

    ブログ本文をtext型へ

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_142227) do
+ActiveRecord::Schema.define(version: 2021_06_04_081216) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.string "image"
-    t.string "content", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
