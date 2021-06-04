565ad9dba806c0062b5961e105caf0236534d0d5
daruo <daruo0830@gmail.com>
2021-06-04 22:16:08 +0900

    セッション系のコントローラー

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にブログ編集ができる
+      log_in(user) # ログイン判定
+      redirect_to "/blog" # リダイレクト
+    else
+      flash[:danger] = 'メールアドレス、パスワードのどちらかまたは両方の入力に誤りがあります。' # エラーメッセージ
+      render '/sessions/new' # loginページ再読み込み
+    end
+  end
+
+  def destroy
+    log_out # logout処理
+    redirect_to "/blog" # 記事一覧へ
+  end
+end
