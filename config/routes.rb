fd4ed47c8a906cbbb9811352bc2acd5cc144e9d5
daruo <daruo0830@gmail.com>
2021-06-04 22:24:05 +0900

    ルート更新

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   delete  '/logout',            to: 'sessions#destroy'  # ログアウト
   
   get     '/blog',              to: 'blog#index'        # ブログ一覧
+  get     '/blog/manage',       to: 'blog#manage'       # ブログ管理画面
   get     '/blog/new',          to: 'blog#new'          # 記事作成
   post    '/blog/create',       to: 'blog#create'       # 投稿処理
+  get     '/blog/:id/status',   to: 'blog#status'       # 記事編集
   get     '/blog/:id/edit',     to: 'blog#edit'         # 記事編集
-  get     '/blog/:id/update',   to: 'blog#update'       # 記事更新
-  destroy '/blog/:id/destroy',  to: 'blog#destroy'      # 記事削除
+  put     '/blog/:id/update',   to: 'blog#update'       # 記事更新
+  delete  '/blog/:id/destroy',  to: 'blog#destroy'      # 記事削除
   
   get     '/contacts',          to: 'contacts#new'      # 問い合わせページ
   post    '/contacts',          to: 'contacts#create'   # 問い合わせ完了処理
