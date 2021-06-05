4b0779b4bcb196f58f919b3cbdf6d9fa43d9aad0
daruo <daruo0830@gmail.com>
2021-06-05 17:48:08 +0900

    お問い合わせ機能を移行

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,30 @@
 class HomeController < ApplicationController
   def top
-    @articles = Article.order("id DESC").limit(6) # 記事を最新最大3件取ってくる
+    @articles = Article.order("id DESC").limit(3) # 記事を最新最大3件取ってくる
+    @contact = Contact.new # お問い合わせの作成準備
   end
   
   def about
 
   end
 
-  def works
-    
+  # contactでつかう
+  def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+          ContactMailer.contact_mail(@contact).deliver
+          flash[:notice] = 'お問い合わせを受け付けました'
+          redirect_to controller: :home, action: :top, notice: 'メッセージが送信されました'
+      else
+          render :new
+      end
   end
+
+  private
+
+  def contact_params
+      params.require(:contact).permit(:name,:email,:message)
+  end
+  
+
 end
