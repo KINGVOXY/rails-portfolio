62bc0f124030c6b757cb2e1658ab028d3009f626
daruo <daruo0830@gmail.com>
2021-06-08 22:27:48 +0900

    メールのバリデーション

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,12 @@
 class Contact < ApplicationRecord
+    before_save { email.downcase! } # user.saveをする前にemailを小文字に変換する
+    
+    validates :name,  presence: true, length: { maximum: 50 } # name最大文字数
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # email正規表現
+    
+    validates :email, presence: true, length: { maximum: 255 }, # email最大文字数
+    format: { with: VALID_EMAIL_REGEX } # emailフォーマット設定
+
+    validates :message, presence: true
+
 end
