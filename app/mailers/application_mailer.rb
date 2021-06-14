af6df3efd09e9efc9fa5755724d29b1881f458a9
daruo <daruo0830@gmail.com>
2021-06-14 17:36:52 +0900

    メーラーの環境設定

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: '管理人 <from@example.com>'
   layout 'mailer'
+
+  after_action :set_envelope_from
+
+  private
+  # envelope-from を設定する
+  # それぞれのメーラーで設定する
+  #
+  def set_envelope_from
+    mail.smtp_envelope_from = 'hogehoge@example.com'
+  end
+
 end
